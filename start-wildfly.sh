fba5e36cfc4e2b4079677bf607c609ce09489bb9
mganez-sesmt <mateusanez@ses.mt.gov.br>
2025-07-09 09:15:57 -0400

    mudança

---
+++ start-wildfly.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#echo "JBOSS_HOME: $JBOSS_HOME"
+
+# Verify JBOSS_HOME is set
+#if [ -z "$JBOSS_HOME" ]; then
+#    echo "Error: JBOSS_HOME environment variable is not set"
+#    exit 1
+#fi
+
+# Change to WildFly bin directory
+cd "bin" || { echo "Failed to enter bin directory"; exit 1; }
+
+# Start WildFly with all configurations
+./standalone.sh \
+  -b 0.0.0.0 \
+  --server-config=standalone-full.xml \
+  -Djboss.server.base.dir="standalone" \
+  -Dprogram.name="JBossTools: WildFly 24+" \
+  -XX:MetaspaceSize=99M -XX:MaxMetaspaceSize=999M \
+  -Dorg.jboss.resolver.warning=true \
+  -Djava.net.preferIPv4Stack=true \
+  -Dsun.rmi.dgc.client.gcInterval=3600000 \
+  -Dsun.rmi.dgc.server.gcInterval=3600000 \
+  -Djboss.modules.system.pkgs=org.jboss.byteman \
+  -Djava.awt.headless=true \
+  -Dorg.jboss.boot.log.file="standalone/log/boot.log" \
+  -Dlogging.configuration="file://standalone/configuration/logging.properties" \
+  -Dorg.jboss.logmanager.nocolor=true \
+  --add-exports=java.desktop/sun.awt=ALL-UNNAMED \
+  --add-exports=java.naming/com.sun.jndi.ldap=ALL-UNNAMED \
+  --add-opens=java.base/java.lang=ALL-UNNAMED \
+  --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
+  --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+  --add-opens=java.base/java.io=ALL-UNNAMED \
+  --add-opens=java.base/java.security=ALL-UNNAMED \
+  --add-opens=java.base/java.util=ALL-UNNAMED \
+  --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+  --add-opens=java.management/javax.management=ALL-UNNAMED \
+  --add-opens=java.naming/javax.naming=ALL-UNNAMED \
+  --add-exports=java.base/sun.nio.ch=ALL-UNNAMED \
+  --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED \
+  --add-exports=jdk.unsupported/sun.reflect=ALL-UNNAMED \
+  --add-modules=java.se \
+  -Djboss.bind.address.management=localhost \
+  -Djboss.as.management.blocking.timeout=9999999 \
+  -Daghu.automatic.timers.enable=true \
+  -Djboss.mail.server.host=smtp.gmail.com \
+  -Djboss.mail.server.port=587 \
+  -Djboss.bind.address=0.0.0.0
